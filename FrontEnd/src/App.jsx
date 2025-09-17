4010945e5ccaff1c8a4b23e51363bbf58323306a
chetnadixit <chetnadixitt@gmail.com>
2025-09-17 11:42:27 +0530

    backend+frontend connection

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Signup from "./pages/SignupPage";
+import Signup from "./pages/Signup";
 // import Navbar from './components/Navbar'
 // import { ThemeProvider } from '@mui/material/styles';
 // import theme from "./theme"; 
